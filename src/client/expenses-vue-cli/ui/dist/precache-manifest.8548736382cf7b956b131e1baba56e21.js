5bf25f9a5b26003f1b9e6d754c9535fc049eae62
egamarra <gamarrafisi@gmail.com>
2020-03-14 08:08:52 -0500

    Dist

---
+++ src/client/expenses-vue-cli/ui/dist/precache-manifest.8548736382cf7b956b131e1baba56e21.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54c838d6abad3f6252ff",
+    "url": "/css/app.93435660.css"
+  },
+  {
+    "revision": "4f6f864c8978d230802b",
+    "url": "/css/chunk-vendors.67042d3d.css"
+  },
+  {
+    "revision": "47b96d9ca143a8858691b370756e5b52",
+    "url": "/human.txt"
+  },
+  {
+    "revision": "118a06525ca79482437b7faece3a3272",
+    "url": "/index.html"
+  },
+  {
+    "revision": "875e8c154e6766444cda",
+    "url": "/js/about.5fa72de5.js"
+  },
+  {
+    "revision": "54c838d6abad3f6252ff",
+    "url": "/js/app.2e97feaa.js"
+  },
+  {
+    "revision": "4f6f864c8978d230802b",
+    "url": "/js/chunk-vendors.976db614.js"
+  },
+  {
+    "revision": "c3c0ef590f45b60b307a006dfc65765a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "/robots.txt"
+  }
+]);
